0a544d55f2fb803cd4fed287bca578a1ba2bc5d7
ramseym4 <ramseym4@nationwide.com>
2022-06-02 16:54:44 -0400

    Fix shellcheck issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,14 @@ if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
     exit 0
 fi
 
-event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
+event_type=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 
 if [ $event_type != "closed" ]; then
     echo "::debug::The event type was '$event_type'"
     exit 0
 fi
 
-milestone_name=$(jq --raw-output .milestone.title $GITHUB_EVENT_PATH)
+milestone_name=$(jq --raw-output .milestone.title "$GITHUB_EVENT_PATH")
 
 IFS='/' read owner repository <<< "$GITHUB_REPOSITORY"
 
